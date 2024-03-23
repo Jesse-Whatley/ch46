3ac27e31cad6efd6c12d153053ab68a8e5b19b88
Jesse-Whatley <crackerculturecollections@gmail.com>
2024-03-23 13:29:06 -0400

    106.1

---
+++ scripts/app.js
@@ -0,0 +1,11 @@
+function sayHello (){
+    console.log("Hello");
+}
+
+
+function init () {
+    console.log("Hello World!");
+    sayHello();
+}
+
+window.onload = init;
---
+++ scripts/notes.js
@@ -0,0 +1,7 @@
+// server is a pc from someone else
+
+//goal is to establish a connection with the server
+
+// in server the . when linking scripts (ex:./srcipts/app.js) means here. It is very import to use the period because it lets the server know i want it to locate the scripts in my project.
+
+// we use window.onload to force the pc to wait 
